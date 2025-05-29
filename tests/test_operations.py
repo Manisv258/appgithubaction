761db1c33a6fd14c29356de6cf5c6b41187decff
Manivannan <manivannan.sv258@gmail.com>
2025-05-30 00:30:23 +0530

    multiplication added

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,4 @@
-from src.math_operations import add,sub
+from src.math_operations import add,sub,multi
 
 def test_add():
     assert add(2,3)==5
@@ -9,3 +9,8 @@ def test_sub():
     assert sub(4,3)==1
     assert sub(3,3)==0
     assert sub(2,3)==-1
+    
+def test_multi():
+    assert multi(2,3)==6
+    assert multi(4,5)==20
+    assert multi(-1,-2)==2
